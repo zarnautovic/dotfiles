15991fa27f82f199c11061f1a9fcff85f578e161
Zlatan Arnautovic <zlatan.arnautovic@sofascore.com>
2026-06-14 19:20:16 +0200

    sketchybar: port front_app to Lua

--- dot_config/sketchybar/init.lua
+++ dot_config/sketchybar/init.lua
@@ -4,3 +4,4 @@ require("default")
 -- Items are wired in over Tasks 3–10 (order preserves on-bar placement):
 -- left:  aerospace, front_app
 -- right: calendar, keyboard, volume, battery, cpu, slack
+require("items.front_app")
