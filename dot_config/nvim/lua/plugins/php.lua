110837683cd6f2abcdfe163763c36ac3d30e7aea
Zlatan Arnautovic <zlatan.arnautovic@sofascore.com>
2026-07-17 09:37:09 +0200

    Add rose-pine colorscheme, update plugin lockfile
    Set rose-pine as the LazyVim colorscheme. Lazy sync also refreshed
    lazy-lock.json (LazyVim moved to blink.cmp/snacks.nvim) and mason.nvim
    references were updated to the new mason-org/ location.

--- dot_config/nvim/lua/plugins/php.lua
+++ dot_config/nvim/lua/plugins/php.lua
@@ -25,7 +25,7 @@ return {
 
   -- Auto-install the PHP tools. list_extend so we don't clobber other ensure_installed entries.
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "php-cs-fixer", "phpstan" })
